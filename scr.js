5d349f50dd518ea23af8adf9db4bf387522cc08c
shimaakamal36 <72226467+shimaakamal36@users.noreply.github.com>
2021-03-31 10:21:54 +0200

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,30 @@
+let todoList=document.querySelector('ul');
+let btn=document.getElementsByTagName('button')[0];
+function addItem(val,parent){
+    let listItem=document.createElement('LI');
+    listItem.innerHTML='<span>' +val +'</span>'+ '<button class="set"> x </button>';
+    parent.appendChild(listItem);
+    let close=listItem.lastElementChild;
+    close.addEventListener('click',function(){
+    this.parentElement.remove();
+}); 
+}
+
+btn.addEventListener('click',(e) => {
+    e.preventDefault();
+    let formInput=document.getElementsByTagName('input')[0].value;
+    if(formInput){
+        addItem(formInput,todoList);
+    }
+});
+let todoData =async function(){
+    let res=await fetch("https://jsonplaceholder.typicode.com/todos");
+    let data= await res.json();
+    // console.log(data);
+    let fetchData=data.slice(0,10);
+    for(var item of fetchData){
+        addItem(item.title,todoList);
+    }
+}
+todoData();
+
